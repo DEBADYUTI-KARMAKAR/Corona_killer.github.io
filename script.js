4a175d60fd7229571a41283018295f0466e43f7b
Debadyuti <debadyuti.karmakar@gmail.com>
2021-08-31 18:08:52 +0530

    Update all fetures

--- script.js
+++ script.js
@@ -1,159 +1,236 @@
-let startBtn = document.querySelector(".start");
-let box = document.querySelector(".box");
-let canvas = document.querySelector(".board");
-let tool = canvas.getContext("2d");
+// yha page se jo element chaiye honge wo saare select kar rhe hai 
+const canvas = document.querySelector(".board");
+const scoreElem = document.querySelector("span");
+const startGame = document.querySelector(".start");
+const box = document.querySelector(".box");
+const powerLevelElem = document.querySelector(".meter span");
 
+const restartBtn = document.querySelector(".restart");
+const tool = canvas.getContext("2d");
+
+const spaceImg = new Image();
+const earth = new Image();
+const corona = new Image();
+spaceImg.src = "space.jpg";
+earth.src = "earth.png";
+corona.src = "corona.png";
+
+canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-canvas.width =window.innerWidth;
+let Bullets = [];
+let particles = [];
+let enemies = [];
+let score = 0;
+let fullPower = 100;
 
-
-let spaceImg = new Image();
-spaceImg.src= "space.jpg";
-let earthImg = new Image();
-earthImg.src= "earth.png";
-let coronaImg = new Image();
-coronaImg.src= "corona.png";
-
-let eHight=40;
-let eWidth=40;
-let ePosX = canvas.width/2-20;
-let ePosY = canvas.height/2-20;
-
-
-class Bullet{
-    constructor(x,y,width,height,velocity){
+class Planet {
+    constructor(x, y, width, height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.velocity =  velocity;
-
-
     }
-    draw(){
-        tool.fillStyle = "gold";
-        tool.fillRect(this.x,this.y,this.width,this.height);
-
+    draw() {
+        tool.beginPath();
+        tool.drawImage(earth, this.x, this.y, this.width, this.height);
     }
-    update(){
+}
+let rectWidth = window.innerWidth / 18;
+let rectHeight = window.innerHeight / 10;
+
+const xPos = (canvas.width / 2) - (rectWidth / 2);
+const yPos = (canvas.height / 2) - (rectHeight / 2);
+const planet = new Planet(xPos, yPos, rectWidth, rectHeight);
+// corona
+class Enemy {
+    constructor(x, y, width, height, velocity) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height
+        this.velocity = velocity;
+    }
+    draw() {
+        tool.beginPath();
+        tool.drawImage(corona, this.x, this.y, this.width, this.height);
+    }
+    update() {
         this.draw();
-        this.x = this.x+this.velocity.x;
-        this.y= this.y+ this.velocity.y;
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
     }
 }
-
-class Corona{
-    constructor(x,y,width,height,velocity){
+class Bullet {
+    constructor(x, y, width, height, color, velocity) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.velocity =  velocity;
-
-
+        this.color = color;
+        this.velocity = velocity;
     }
-    draw(){
-        //tool.fillStyle = "gold";
-        tool.drawImage(coronaImg ,this.x,this.y,this.width,this.height);
-
+    draw() {
+        tool.beginPath();
+        tool.fillStyle = this.color;
+        tool.fillRect(this.x, this.y, this.width, this.height);
     }
-    update(){
+    update() {
         this.draw();
-        this.x = this.x+this.velocity.x;
-        this.y= this.y+ this.velocity.y;
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
     }
 }
-
-
-let bullets = [];
-let coronas = [];
-
-
-class Plant {
-    constructor(x,y,width,height){
+class Particle {
+    constructor(x, y, radius, color, velocity) {
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height = height;
-
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+        this.alpha = 1;
     }
-    draw(){
-
-        tool.drawImage(earthImg,this.x,this.y,this.width,this.height);
+    draw() {
+        tool.save();
+        tool.globalAlpha = this.alpha;
+        tool.beginPath();
+        tool.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        tool.fillStyle = this.color;
+        tool.fill();
+        tool.restore();
+    }
+    update() {
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+        this.alpha -= 0.01;
     }
 }
 
-
-function animate(){
-    tool.clearRect(0,0,canvas.width,canvas.height);
-    tool.fillRect(0,0,canvas.width,canvas.height);
-    tool.drawImage(spaceImg,0,0,canvas.width,canvas.height);
-
-    let earth = new Plant(ePosX,ePosY,eWidth,eHight);
-
-    earth.draw();
-    let bLength = bullets.length;
-    for(let i=0;i<bLength;i++){
-        bullets[i].update();
-        if(bullets[i].x<0 || bullets[i].y<0 || bullets[i].x>canvas.width || bullets[i].y>canvas.height){
-            setTimeout(function(){
-
-                bullets.splice(i,1);
+function spawnEnemies() {
+    setInterval(() => {
+        const delta = 20;
+        
+        let x;
+        let y;
+        
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? delta : canvas.width + delta * 2;
+            y = Math.random() * canvas.height;
+        } else {
+            x = Math.random() * canvas.width;
+            y = Math.random() < 0.5 ? delta : canvas.height + delta * 2;
+        }
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+        enemies.push(new Enemy(x, y, 40, 40, velocity));
+    }, 1000);
+}
+let animationId;
+function animate() {
+    animationId = requestAnimationFrame(animate);
+    tool.clearRect(0, 0, canvas.width, canvas.height);
+    tool.drawImage(spaceImg, 0, 0, canvas.width, canvas.height);
+    planet.draw();
+    
+    particles.forEach((particle, index) => {
+        if (particle.alpha <= 0) {
+            particles.splice(index, 1);
+        } else {
+            particle.update();
+        }
+    });
+    
+    Bullets.forEach((Bullet, index) => {
+        Bullet.update();
+        
+        if (Bullet.x - Bullet.width < 0
+            || Bullet.x - Bullet.width > canvas.width
+            || Bullet.y + Bullet.width < 0 ||
+            Bullet.y - Bullet.width > canvas.height
+        ) {
+            setTimeout(() => {
+                Bullets.splice(index, 1);
             })
         }
-    }
-    let cLength = coronas.length;
-    for(let i=0;i<cLength;i++){
-        coronas[i].update();
-        
-        
-    }
-    
-    requestAnimationFrame(animate);
-}
-
-function createCorona(){
-    setInterval(function(){
-        let x= Math.random()*canvas.width;
-        let y=Math.random()*canvas.height;
-        let delta = Math.random();
-        if(delta<0.5){
-            x=Math.random()<0.5?0:canvas.width;
-            y = Math.random()*canvas.height;
-        }else{
-            y=Math.random()<0.5?0:canvas.height;
-            x = Math.random()*canvas.width;
-        }
-        let angle = Math.atan2(canvas.height/2-y, canvas.width/2-x);
-        let velocity ={
-            x:Math.cos(angle)*3,
-            y:Math.sin(angle)*3
-
-        }
-        let corona = new Corona(x,y,25,25,velocity);
-        coronas.push(corona);
-    },1000)
-}
-startBtn.addEventListener("click",function(e){
-e.stopImmediatePropagation();
-    //alert("Start the game")
-    box.style.display = "none";
-
-    
-    animate()
-    createCorona();
-    window.addEventListener("click",function(e){
-        console.log(e);
-        console.log("mouse clocked");
-        let angle = Math.atan2(e.clientY-canvas.height/2,e.clientX-canvas.width/2);
-        let velocity ={
-            x:Math.cos(angle)*3,
-            y:Math.sin(angle)*3
-
-        }
-        let bullet = new Bullet(canvas.width/2,canvas.height/2,7,7,velocity)
-        bullet.draw();
-        bullets.push(bullet)
     })
+   
+    enemies.forEach((enemy, index) => {
+        enemy.update();
+        
+        if (collisonRec(planet.x, planet.y, planet.width, planet.height, enemy.x, enemy.y, enemy.width, enemy.height)) {
+            fullPower -= 20;
+            powerLevelElem.style.width = `${fullPower}%`;
+            enemies.splice(index, 1);
+            if (!fullPower) {
+                console.log("end game");
+               
+                cancelAnimationFrame(animationId)
+                restart()
+            }
+        }
+       
+        Bullets.forEach((Bullet, BulletIndex) => {
+            
+            if (collisonRec(enemy.x, enemy.y, enemy.width, enemy.height, Bullet.x, Bullet.y, Bullet.width, Bullet.height)) {
+                
+                for (let i = 0; i < enemy.width * 2; i++) {
+                    particles.push(new Particle(Bullet.x, Bullet.y, Math.random() * 2, "white", {
+                        x: (Math.random() - 0.5) * Math.random() * 6,
+                        y: (Math.random() - 0.5) * Math.random() * 6
+                    }))
+                }
 
+                setTimeout(() => {
+                    enemies.splice(index, 1);
+                    score += 100;
+                    scoreElem.innerText = score;
+                    Bullets.splice(BulletIndex, 1);
+                }, 0)
+            } else {
+            }
+        })
+    })
+}
+function init() {
+    
+    animate();
+    spawnEnemies();
+    
+    addEventListener('click', (event) => {
+        const angle = Math.atan2(event.clientY - canvas.height / 2
+            , event.clientX - canvas.width / 2)
+        const velocity = {
+            x: Math.cos(angle) * 4.5,
+            y: Math.sin(angle) * 4.5
+        }
+        Bullets.push(new Bullet(canvas.width / 2, canvas.height / 2, 8, 8, 'white', velocity))
+    })
+}
+startGame.addEventListener("click", function () {
+    box.style.display = "none";
+    init();
 })
+
+window.addEventListener("resize", function () {
+    window.location.reload();
+})
+function collisonRec(r1x, r1y, r1w, r1h, r2x, r2y, r2w, r2h) {
+    if (r1x + r1w >= r2x && r1x <= r2x + r2w && r1y + r1h >= r2y && r1y <= r2y + r2h) {
+        return true
+    }
+    return false;
+}
+function restart() {
+    restartBtn.style.display = "block";
+    startGame.style.display="none";
+    box.style.display = "block";
+    canvas.height="0px";
+    document.body.style.backgroundColor="white";
+    powerLevelElem.parentElement.style.display="none";
+    restartBtn.addEventListener("click", function () {
+document.location.reload();
+    })
+}
